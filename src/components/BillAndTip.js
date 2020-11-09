0b64af441789941454ecd87f1abd275ece162295
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-11-09 13:03:22 +1000

    added clear button

--- src/components/BillAndTip.js
+++ src/components/BillAndTip.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react"
 
 import "../styles/BillAndTip.css"
+import {Container} from './Styled'
+
+
 
 class BillAndTip extends Component {
     constructor(props){
@@ -25,30 +28,37 @@ class BillAndTip extends Component {
 
         genTip = (event) => {
             event.preventDefault();   
-            let tip = parseFloat((this.state.tipPercentage/ 100) * this.state.total).toFixed(2)
-            let total = parseFloat(this.state.total + tip)
+            let tip = parseFloat((this.state.tipPercentage/ 100) * this.state.total)
+            let total = parseFloat(this.state.total/100 + tip)
             this.setState({
                 tipFigure: tip,
                 billTotal: total
             })
         }
+        clearCache = (event) => {
+            window.location.reload(false);
+        }
 
     render(){ 
       return(
-            <div>    
+            <div>   
+            <Container>
+                    <h1>Bill & Tip</h1>
                         <form onSubmit={this.genTip}>
                             <label>
-                            Bill Total:
+                            Bill Total: $
                             <input type="text" value={this.state.total} onChange={this.handleTotal} />
                             </label>
                             <label>
-                            Tip Percentage:
-                            <input type="text" value={this.state.tipPercentage} onChange={this.handleTip} />%
+                            Tip Percentage: %
+                            <input type="text" value={this.state.tipPercentage} onChange={this.handleTip} />
                             </label>
                             <input type="submit" value="Submit" />
                         </form>
                         <p>Tip Amount: ${this.state.tipFigure}</p>
                         <p>Bill Total: ${this.state.billTotal}</p>
+                        <button onClick={this.clearCache}>Clear</button>
+            </Container> 
                 </div>
             )
         }
---
+++ src/components/Styled.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+
+
+export const Container = styled.div `
+
+margin: 0 auto;
+margin-top: 50px;
+text-align: center;
+display: flex;
+justify-content: center;
+flex-direction: column;
+flex-wrap: wrap;
+font-size: 1em;
+padding: 20px;
+max-width: 400px;
+border: 1px solid black;
+h1 {
+    color: green;
+}
+form {
+ 
+    label {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+        padding: 10px;
+        input {
+            margin: 10px; 
+        }
+    }
+}
+p {
+    color: green;
+}
+button {
+    width: 50px;
+     align-self: center;
+}
+`
