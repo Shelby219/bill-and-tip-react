3aee86669944b0e06ac21814577f081886a2b87c
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-11-10 08:40:12 +1000

    updated commit

--- src/components/Styled.js
+++ src/components/Styled.js
@@ -6,7 +6,7 @@ export const Container = styled.div `
 
 margin: 0 auto;
 margin-top: 50px;
-text-align: center;
+text-align: center;     
 display: flex;
 justify-content: center;
 flex-direction: column;
